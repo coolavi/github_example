6022af3674285a6da67b4bb0e59d40809e2042f9
coolavi <savinash202@gmail.com>
2017-06-05 12:13:51 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
-    cout << "github" << endl;
+  int n,i,j,p;
+
+  cout<<"\nenter the number of rows or order of the matrix=>";
+  cin>>n;
+    char ch[n];
+    for(i=0;i<n;i++)
+        ch[i]='A'+i;
+p=n;
+
+    for(i=0;i<n;i++)
+  {
+           for(j=n-1;j>=0;j--)
+      {
+        if(i>=j)
+        {
+          cout<<ch[p-1];
+            p--;
+        }
+        else p=n;
+      }
+      p=n;
+
+
+      cout<<"\n";
+
+  }
+
     return 0;
 }
